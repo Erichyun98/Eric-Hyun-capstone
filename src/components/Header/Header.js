b08914568f965cfc7f5d2289dd5eb002f2c3da18
Eric Hyun <tmddlr75@gmail.com>
2022-03-31 14:42:15 -0400

    implemented basic header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.scss'
+
+function Header () { 
+    return (
+       <section className='header'> 
+           {/* <img src = {logo} className='header__logo'/>  */}
+            <h1 className='header__title'>AccountMe</h1>
+       </section>
+    )
+}
+export default Header;
